9a53091fee55c0ff0dc419525a754948ddfcee5a
Ciel Yang <cielsk43@gmail.com>
2017-04-05 08:34:57 +0800

    First commit.

---
+++ demo/src/main/java/com/cielyang/android/customedittext/MainActivity.java
@@ -0,0 +1,12 @@
+package com.cielyang.android.customedittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
