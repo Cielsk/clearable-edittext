b4e60587528ceb4cbb0dd23de9883eeb7ce5eb49
Ciel Yang <cielsk43@gmail.com>
2018-08-10 16:05:31 +0800

    change project code format to AOSP style

--- clearableedittext/src/main/java/com/cielyang/android/clearableedittext/ClearableEditText.java
+++ clearableedittext/src/main/java/com/cielyang/android/clearableedittext/ClearableEditText.java
@@ -15,167 +15,154 @@ import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
-/**
- *
- */
+/** */
 public class ClearableEditText extends AppCompatEditText implements TextWatcher {
 
-  @DrawableRes
-  private static final int DEFAULT_CLEAR_ICON_RES_ID = R.drawable.ic_clear;
+    @DrawableRes private static final int DEFAULT_CLEAR_ICON_RES_ID = R.drawable.ic_clear;
 
-  private Drawable mClearIconDrawable;
+    private Drawable mClearIconDrawable;
 
-  private boolean mIsClearIconShown = false;
+    private boolean mIsClearIconShown = false;
 
-  private boolean mClearIconDrawWhenFocused = true;
+    private boolean mClearIconDrawWhenFocused = true;
 
-  public ClearableEditText(Context context) {
-    this(context, null);
-  }
-
-  public ClearableEditText(Context context, AttributeSet attrs) {
-    this(context, attrs, android.R.attr.editTextStyle);
-  }
-
-  public ClearableEditText(Context context, AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-    init(attrs, defStyleAttr);
-  }
-
-  private void init(AttributeSet attrs, int defStyle) {
-    // Load attributes
-    final TypedArray a =
-        getContext().obtainStyledAttributes(attrs, R.styleable.ClearableEditText, defStyle, 0);
-
-    if (a.hasValue(R.styleable.ClearableEditText_clearIconDrawable)) {
-      mClearIconDrawable = a.getDrawable(R.styleable.ClearableEditText_clearIconDrawable);
-      if (mClearIconDrawable != null) {
-        mClearIconDrawable.setCallback(this);
-      }
+    public ClearableEditText(Context context) {
+        this(context, null);
     }
 
-    mClearIconDrawWhenFocused = a
-        .getBoolean(R.styleable.ClearableEditText_clearIconDrawWhenFocused, true);
-
-    a.recycle();
-  }
-
-  @Override
-  public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-    // no operation
-  }
-
-  @Override
-  public void afterTextChanged(Editable s) {
-  }
-
-  @Override
-  public Parcelable onSaveInstanceState() {
-    Parcelable superState = super.onSaveInstanceState();
-    return mIsClearIconShown ? new ClearIconSavedState(superState, true)
-        : superState;
-  }
-
-  @Override
-  public void onRestoreInstanceState(Parcelable state) {
-    if (!(state instanceof ClearIconSavedState)) {
-      super.onRestoreInstanceState(state);
-      return;
-    }
-    ClearIconSavedState savedState = (ClearIconSavedState) state;
-    super.onRestoreInstanceState(savedState.getSuperState());
-    mIsClearIconShown = savedState.isClearIconShown();
-    showClearIcon(mIsClearIconShown);
-  }
-
-  @Override
-  public void onTextChanged(CharSequence s, int start, int before, int count) {
-    if (hasFocus()) {
-      showClearIcon(!TextUtils.isEmpty(s));
-    }
-  }
-
-  @Override
-  protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
-    showClearIcon(
-        (!mClearIconDrawWhenFocused || focused) && !TextUtils.isEmpty(getText().toString()));
-    super.onFocusChanged(focused, direction, previouslyFocusedRect);
-  }
-
-  @SuppressLint("ClickableViewAccessibility")
-  @Override
-  public boolean onTouchEvent(MotionEvent event) {
-    if (isClearIconTouched(event)) {
-      setText(null);
-      event.setAction(MotionEvent.ACTION_CANCEL);
-      showClearIcon(false);
-      return false;
-    }
-    return super.onTouchEvent(event);
-  }
-
-  private boolean isClearIconTouched(MotionEvent event) {
-    if (!mIsClearIconShown) {
-      return false;
+    public ClearableEditText(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.editTextStyle);
     }
 
-    final int touchPointX = (int) event.getX();
-
-    final int widthOfView = getWidth();
-    final int compoundPaddingRight = getCompoundPaddingRight();
-
-    return touchPointX >= widthOfView - compoundPaddingRight;
-  }
-
-  private void showClearIcon(boolean show) {
-    if (show) {
-      // show icon on the right
-      if (mClearIconDrawable != null) {
-        setCompoundDrawablesWithIntrinsicBounds(null, null, mClearIconDrawable, null);
-      } else {
-        setCompoundDrawablesWithIntrinsicBounds(0, 0, DEFAULT_CLEAR_ICON_RES_ID, 0);
-      }
-    } else {
-      // remove icon
-      setCompoundDrawables(null, null, null, null);
-    }
-    mIsClearIconShown = show;
-  }
-
-  protected static class ClearIconSavedState extends BaseSavedState {
-
-    public static final Creator<ClearIconSavedState> CREATOR =
-        new Creator<ClearIconSavedState>() {
-          @Override
-          public ClearIconSavedState createFromParcel(Parcel source) {
-            return new ClearIconSavedState(source);
-          }
-
-          @Override
-          public ClearIconSavedState[] newArray(int size) {
-            return new ClearIconSavedState[size];
-          }
-        };
-    private final boolean mIsClearIconShown;
-
-    private ClearIconSavedState(Parcel source) {
-      super(source);
-      mIsClearIconShown = source.readByte() != 0;
+    public ClearableEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs, defStyleAttr);
     }
 
-    ClearIconSavedState(Parcelable superState, boolean isClearIconShown) {
-      super(superState);
-      mIsClearIconShown = isClearIconShown;
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a =
+            getContext().obtainStyledAttributes(attrs, R.styleable.ClearableEditText, defStyle, 0);
+
+        if (a.hasValue(R.styleable.ClearableEditText_clearIconDrawable)) {
+            mClearIconDrawable = a.getDrawable(R.styleable.ClearableEditText_clearIconDrawable);
+            if (mClearIconDrawable != null) {
+                mClearIconDrawable.setCallback(this);
+            }
+        }
+
+        mClearIconDrawWhenFocused =
+            a.getBoolean(R.styleable.ClearableEditText_clearIconDrawWhenFocused, true);
+
+        a.recycle();
+    }
+
+    @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // no operation
+    }
+
+    @Override public void afterTextChanged(Editable s) {
+    }
+
+    @Override public Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+        return mIsClearIconShown ? new ClearIconSavedState(superState, true) : superState;
+    }
+
+    @Override public void onRestoreInstanceState(Parcelable state) {
+        if (!(state instanceof ClearIconSavedState)) {
+            super.onRestoreInstanceState(state);
+            return;
+        }
+        ClearIconSavedState savedState = (ClearIconSavedState) state;
+        super.onRestoreInstanceState(savedState.getSuperState());
+        mIsClearIconShown = savedState.isClearIconShown();
+        showClearIcon(mIsClearIconShown);
+    }
+
+    @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (hasFocus()) {
+            showClearIcon(!TextUtils.isEmpty(s));
+        }
     }
 
     @Override
-    public void writeToParcel(Parcel out, int flags) {
-      super.writeToParcel(out, flags);
-      out.writeByte((byte) (mIsClearIconShown ? 1 : 0));
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        showClearIcon(
+            (!mClearIconDrawWhenFocused || focused) && !TextUtils.isEmpty(getText().toString()));
+        super.onFocusChanged(focused, direction, previouslyFocusedRect);
     }
 
-    boolean isClearIconShown() {
-      return mIsClearIconShown;
+    @SuppressLint("ClickableViewAccessibility") @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (isClearIconTouched(event)) {
+            setText(null);
+            event.setAction(MotionEvent.ACTION_CANCEL);
+            showClearIcon(false);
+            return false;
+        }
+        return super.onTouchEvent(event);
     }
-  }
+
+    private boolean isClearIconTouched(MotionEvent event) {
+        if (!mIsClearIconShown) {
+            return false;
+        }
+
+        final int touchPointX = (int) event.getX();
+
+        final int widthOfView = getWidth();
+        final int compoundPaddingRight = getCompoundPaddingRight();
+
+        return touchPointX >= widthOfView - compoundPaddingRight;
+    }
+
+    private void showClearIcon(boolean show) {
+        if (show) {
+            // show icon on the right
+            if (mClearIconDrawable != null) {
+                setCompoundDrawablesWithIntrinsicBounds(null, null, mClearIconDrawable, null);
+            } else {
+                setCompoundDrawablesWithIntrinsicBounds(0, 0, DEFAULT_CLEAR_ICON_RES_ID, 0);
+            }
+        } else {
+            // remove icon
+            setCompoundDrawables(null, null, null, null);
+        }
+        mIsClearIconShown = show;
+    }
+
+    protected static class ClearIconSavedState extends BaseSavedState {
+
+        public static final Creator<ClearIconSavedState> CREATOR =
+            new Creator<ClearIconSavedState>() {
+                @Override public ClearIconSavedState createFromParcel(Parcel source) {
+                    return new ClearIconSavedState(source);
+                }
+
+                @Override public ClearIconSavedState[] newArray(int size) {
+                    return new ClearIconSavedState[size];
+                }
+            };
+        private final boolean mIsClearIconShown;
+
+        private ClearIconSavedState(Parcel source) {
+            super(source);
+            mIsClearIconShown = source.readByte() != 0;
+        }
+
+        ClearIconSavedState(Parcelable superState, boolean isClearIconShown) {
+            super(superState);
+            mIsClearIconShown = isClearIconShown;
+        }
+
+        @Override public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeByte((byte) (mIsClearIconShown ? 1 : 0));
+        }
+
+        boolean isClearIconShown() {
+            return mIsClearIconShown;
+        }
+    }
 }
